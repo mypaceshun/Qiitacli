57851b48cc0754022cd5a7ccdebbe306356c5421
KAWAI Shun <shun@osstech.co.jp>
2019-10-29 19:20:11 +0900

    Added MANIFEST.in

--- qiitacli/__init__.py
+++ qiitacli/__init__.py
@@ -1,7 +1,8 @@
 import os
 
 PRODUCT_NAME = 'qiitacli'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ACCESSTOKEN_PATH = os.environ.get(
-    "QIITACLI_ACCESSTOKEN_PATH", "/path/to/etc/accesstoken.secret")
+    "QIITACLI_ACCESSTOKEN_PATH", os.path.join(BASE_DIR, "etc", "accesstoken.secret"))
 
 __version__ = '0.1.0'
