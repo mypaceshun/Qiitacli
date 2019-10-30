16162e9be0f2b6f022fea57647a8ca4600160200
KAWAI Shun <shun@osstech.co.jp>
2019-10-30 11:02:12 +0900

    アクセストークンの保存先を変更

--- qiitacli/__init__.py
+++ qiitacli/__init__.py
@@ -1,8 +1,4 @@
-import os
-
 PRODUCT_NAME = 'qiitacli'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ACCESSTOKEN_PATH = os.environ.get(
-    "QIITACLI_ACCESSTOKEN_PATH", os.path.join(BASE_DIR, "etc", "accesstoken.secret"))
+ACCESSTOKEN_PATH = os.environ.get("QIITACLI_ACCESSTOKEN_PATH", "~/.{}.secret".format(PRODUCT_NAME))
 
 __version__ = '0.1.0'
